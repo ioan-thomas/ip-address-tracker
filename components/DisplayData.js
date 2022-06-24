1a046bc4ac187b9e4efbb64773041def22e91591
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-24 16:04:49 +0100

    feat: outputting retrieved data to ui

--- components/DisplayData.js
+++ components/DisplayData.js
@@ -1,14 +1,29 @@
-import { useState, useEffect } from "react"
-
-export default function DisplayData(response){
-
-    console.log(response)
-    
-
-
+export default function DisplayData({response, error}){
 
    return (
-     <div></div>
+
+    <div>
+        {response && 
+        <>
+            <div className="data-container border">
+                <h6 className="data-title">IP ADDRESS</h6>
+                <h4>{response.ip}</h4>
+            </div>
+            <div className="data-container border">
+                <h6 className="data-title">LOCATION</h6>
+                <h4>{`${response.location.city}, ${response.location.region}, ${response.location.country}`}</h4>
+            </div>
+            <div className="data-container border">
+                <h6 className="data-title">TIMEZONE</h6>
+                <h4>{`UTC ${response.location.timezone}`}</h4>
+            </div>
+            <div className="data-container">
+                <h6 className="data-title">ISP</h6>
+                <h4>{response.isp}</h4>
+            </div>
+        </>}
+        {error && <p>{error}</p>}
+     </div>
    )
  }
  
