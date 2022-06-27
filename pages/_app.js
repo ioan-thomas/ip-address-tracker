fb04ad265509f1315eaac4383a0dd8aa82b1508e
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-27 12:21:30 +0100

    feat: added global styles with styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import {GlobalStyle} from '../components/styles/GlobalStyle.styled'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
